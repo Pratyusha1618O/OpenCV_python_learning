c2933edeb7bbe05f3d640f0e137307862965f568
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-02-10 00:26:20 +0530

    Rotation

--- opencv_tutorial/Rotation/rotate45.py
+++ opencv_tutorial/Rotation/rotate45.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread("flower.jpg")
 
-# rotate 45
-center = (image.shape[1] // 2, image.shape[0] // 2)
-rotation_45_clock = cv2.getRotationMatrix2D(center, -45, 1) #rotate -45 clockwise
-rotation_45_anticlock = cv2.getRotationMatrix2D(center, 45, 1) #rotate 45 clockwise
+r = image.shape[0]
+c = image.shape[1]
+center = (c//2, r//2)
 
-rotated_img_clock = cv2.warpAffine(image, rotation_45_clock, (image.shape[1], image.shape[0]))
+# clock wise
+rotation_matrix_clock = cv2.getRotationMatrix2D(center, -45, 1)
+clock_img = cv2.warpAffine(image, rotation_matrix_clock, (c, r))
 
-rotate_img_anticlock = cv2.warpAffine(image, rotation_45_anticlock, (image.shape[1], image.shape[0]))
+# Anti clock wise
+rotation_matrix_anticlock = cv2.getRotationMatrix2D(center, 45, 1)
+anticlock_img = cv2.warpAffine(image, rotation_matrix_anticlock, (c, r))
 
 
 plt.figure(figsize=(10, 5))
@@ -20,12 +23,12 @@ plt.title("Original")
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
 plt.subplot(1,3,2)
-plt.title("Clockwise 90")
-plt.imshow(cv2.cvtColor(rotated_img_clock, cv2.COLOR_BGR2RGB))
+plt.title("Clockwise 45")
+plt.imshow(cv2.cvtColor(clock_img, cv2.COLOR_BGR2RGB))
 
 plt.subplot(1,3,3)
-plt.title("Anticlockwise 90")
-plt.imshow(cv2.cvtColor(rotate_img_anticlock, cv2.COLOR_BGR2RGB))
+plt.title("Anticlockwise 45")
+plt.imshow(cv2.cvtColor(anticlock_img, cv2.COLOR_BGR2RGB))
 
 plt.show()
 
