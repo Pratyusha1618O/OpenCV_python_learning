7af79a3b606a71aa6b74f191af42db53dca320a6
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-02-09 21:57:23 +0530

    Remove R G B from image

---
+++ opencv_tutorial/08_remove_RGB.py
@@ -0,0 +1,23 @@
+import cv2
+image = cv2.imread("pic.jpg")
+
+if image is None:
+    print("Error: Could not read the image.")
+    exit()
+
+# Remove colors by setting channels to 0
+no_red = image.copy()
+no_red[:, :, 2] = 0  # Remove Red
+
+no_blue = image.copy()
+no_blue[:, :, 0] = 0  # Remove Blue
+
+no_green = image.copy()
+no_green[:, :, 1] = 0  # Remove Green
+
+# Display images
+for title, img in zip(["Original", "No Red", "No Blue", "No Green"], [image, no_red, no_blue, no_green]):
+    cv2.imshow(title, img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
